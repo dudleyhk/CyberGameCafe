da629036f477874ac50f659c44f50064efe8a7a9
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-06-12 18:13:06 +0100

    weight checker added and test weighted sprite added to scene

--- Assets/Scripts/Pathfinding/Node.cs
+++ Assets/Scripts/Pathfinding/Node.cs
@@ -10,7 +10,14 @@ public class Node
 {
     public Node      ParentNode  { get; internal set; }
     public Vector3   Centre      { get; internal set; }
-    public bool      Weight      { get; internal set; }
+    public int       Weight      { get; internal set; }
+
+
+    enum WeightType
+    {
+        None,
+        Static
+    }
 
 
     public Node(Vector3 centre)
@@ -20,7 +27,7 @@ public class Node
     }
 
     /// <summary>
-    /// Calculate a weight for each node based on if any objects are over that tile. 
+    /// Calculate a weight for each node based on if any sprites are over that node. 
     /// </summary>
     private void CalculateWeight()
     {
@@ -28,17 +35,30 @@ public class Node
 
         float dir = cameraPos.z - Centre.z;
         Vector3 rayDirection = new Vector3(0, 0, dir);
+        Debug.DrawRay(Centre, rayDirection, Color.yellow, 50);
 
 
-        RaycastHit hitInfo;
-        Ray ray = new Ray(Centre, rayDirection);
-        int ignoreRaycast = 2;
-
-        if(Physics.Raycast(ray, out hitInfo, ignoreRaycast))
+        RaycastHit2D[] infoArray = Physics2D.RaycastAll(Centre, rayDirection, 5.0f);
+        foreach(var hitInfo in infoArray)
         {
-            // check if it hits any other sprites.
-
-           // try adding a sprite into the scene and see if the raycast works
+            Weight = GetWeight(hitInfo.transform.tag);
         }
+    }
+
+
+    /// <summary>
+    /// Return the enum value for the type of sprite which has been hit. 
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    private int GetWeight(string tag)
+    {
+        switch(tag)
+        {
+            case "StaticSprite":
+                return System.Convert.ToInt32(WeightType.Static);
+        }
+        return System.Convert.ToInt32(WeightType.None);
+        
     }
 }
