3ca59872efcb8c3b2ba04e17a27cbfcdf91f8805
Huw Tingley <Huw2.Tingley@live.uwe.ac.uk>
2017-08-23 10:24:57 +0100

    updating charlies version to master with sprites and prefabs

---
+++ Assets/Scripts/player_animation_script.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_animation_script : MonoBehaviour
+{
+
+    [SerializeField] Animator anim;
+
+    private Vector3 lastPosition;
+    private Vector3 currentPosition;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        float horizontal = 0;
+        float vertical = 0;
+
+        currentPosition = transform.position;
+
+        if(currentPosition.x > lastPosition.x)
+        {
+            horizontal = 1;
+        }
+        else if (currentPosition.x < lastPosition.x)
+        {
+            horizontal = -1;
+        }
+        else if (currentPosition.y > lastPosition.y)
+        {
+            vertical = 1;
+        }
+        else if (currentPosition.y < lastPosition.y)
+        {
+            vertical = -1;
+        }
+
+        lastPosition = currentPosition;
+
+        AnimatorUpdate(horizontal, vertical);
+
+    }
+
+    void AnimatorUpdate(float horizontal, float vertical)
+    {
+        anim.SetFloat("x", horizontal);
+        anim.SetFloat("y", vertical);
+    }
+
+}
