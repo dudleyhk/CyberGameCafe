3fb4373031773b4fb2fd171bc7933174f15ba4a7
Thomas Sylvester <Thomas2.Sylvester@live.uwe.ac.uk>
2017-09-15 16:26:30 +0100

    Encryption quest

---
+++ Assets/Scripts/Quests/EncryptionQuest/TriggerEncrptionQuest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEncrptionQuest : MonoBehaviour
+{
+    private GameObject player;
+    private bool playerInBox;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerInBox == true && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Scene Switch to Encryption");
+            Application.LoadLevel(1);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (col.gameObject == player)
+        {
+            Debug.Log("playing in encrpytion trigger");
+            playerInBox = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (col.gameObject == player)
+        {
+            playerInBox = false;
+        }
+    }
+}
