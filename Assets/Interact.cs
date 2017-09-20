34ee3edcd4466474ac3119f3953d5316b542419f
TwelveSharks <tomsylvester1@yahoo.co.uk>
2017-09-20 16:32:55 +0100

    E key now advances text and no longer queues up additional text boxes

---
+++ Assets/Interact.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+	bool enabled;
+	public bool beingPressed;
+
+	void Start()
+	{
+		enabled = true;
+	}
+
+	void Update () 
+	{
+		if (enabled && Input.GetKeyDown (KeyCode.E)) {
+			beingPressed = true;
+		}
+		else
+		{
+			beingPressed = false;
+		}
+	}
+
+	public void toggleInteraction()
+	{
+		enabled = !enabled;
+	}
+}
