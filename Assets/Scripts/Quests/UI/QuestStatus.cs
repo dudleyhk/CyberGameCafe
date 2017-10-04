91627c4f84015a923cdb1836ca44808ee122149f
n86-64 <Nathan2.Butt@live.uwe.ac.uk>
2017-10-04 16:24:37 +0100

    Added Quest UI script
    Added a new script template called QuestStatus. Attached the QuestStatus script to the corrisponding UI component and added the basic shell of the script (TODO - add an algorithm to draw and update the UI)

---
+++ Assets/Scripts/Quests/UI/QuestStatus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestStatus : MonoBehaviour {
+
+    private GameObject questStatusUI;
+
+    // Comprises a row in the panel. Displays the compleated quests in a list
+    public GameObject questName;
+    public GameObject questCompleteIcon;
+
+	// Use this for initialization
+	void Awake()
+    {
+        questStatusUI = GameObject.FindGameObjectWithTag("QuestPanel");		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void createQuestDisplay()
+    {
+
+    }
+}
