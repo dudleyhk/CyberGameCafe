632c701dab2727dbb0937961b7aa1fb6b2e6ec01
Tingoles <Huw.Tingley@gmail.com>
2017-10-04 13:52:34 +0100

    creating initial newPhishingQuest Scene

---
+++ Assets/Scripts/Quests/NewPhishingQuest/UnparentCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnparentCamera : MonoBehaviour {
+
+    private GameObject camera;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        if (camera.transform.parent != null)
+        {
+            Debug.Log("aa");
+            camera.transform.parent = null;
+            camera.transform.Translate(10, 0, 0);
+        }
+	}
+}
