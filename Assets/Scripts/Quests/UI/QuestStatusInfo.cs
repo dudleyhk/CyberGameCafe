162657cf0164b7b2a0b89c582141680e12bb1bce
n86-64 <Nathan2.Butt@live.uwe.ac.uk>
2017-10-11 16:11:55 +0100

    Created Quest Panel Base Logic.
    Created a basic routiene which based on input will create a list of the
    quests that the player has either compleated or has to complete. (TODO -
    Create logic for updating the list and refine the UI to improve apperance.)

---
+++ Assets/Scripts/Quests/UI/QuestStatusInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestStatusInfo : MonoBehaviour {
+
+    public string QuestName;
+    private bool QuestComplete = false;
+
+    public void isQuestComplete(bool value)
+    {
+        QuestComplete = value;
+    }
+}
